5f632734245c447bc7274f19f09f07c5e43385e8
Laura <lauramaisele@hotmail.lv>
2024-02-16 10:29:08 +0200

    ADD: added EmployeeDTO class with fields

---
+++ src/main/java/eu/virac/dlut/models/helpers/EmployeeDTO.java
@@ -0,0 +1,30 @@
+package eu.virac.dlut.models.helpers;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EmployeeDTO {
+
+    @NotNull
+    //viens vārds, divi vārdi ar defisi, divi vārdi ar atstarpi
+    @Pattern(regexp = "^\\p{Lu}{1}\\p{Ll}+( \\p{Lu}{1}\\p{Ll}+|-\\p{Lu}{1}\\p{Ll}+)?$")
+    private String name;
+
+    @NotNull
+    //viens vārds, divi vārdi ar defisi, divi vārdi ar atstarpi
+    @Pattern(regexp = "^\\p{Lu}{1}\\p{Ll}+( \\p{Lu}{1}\\p{Ll}+|-\\p{Lu}{1}\\p{Ll}+)?$")
+    private String surname;
+
+    @NotNull
+    private String positionTitle;
+
+    @NotNull
+    private String departmentTitle;
+}
