995732db247c618853b0e6c7bc6fdb41aa936221
Laura <lauramaisele@hotmail.lv>
2024-02-29 14:17:32 +0200

    EDIT: Change IOrderMissionRepo from regular query to sql procedure

--- src/main/java/eu/virac/dlut/repos/IOrderMissionRepo.java
+++ src/main/java/eu/virac/dlut/repos/IOrderMissionRepo.java
@@ -4,17 +4,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import eu.virac.dlut.models.OrderMission;
+import org.springframework.data.repository.query.Param;
 
 
 public interface IOrderMissionRepo extends CrudRepository<OrderMission, Integer> {
 
-	@Query(value = "SELECT * FROM order_mission om "
-			+ "JOIN employee_time_sheet ets ON om.id_order = ets.id_ord_mission "
-			+ "WHERE (EXTRACT(DAY FROM ets.year_month_day) = ?1 AND EXTRACT(MONTH FROM ets.year_month_day) = ?2 AND EXTRACT(YEAR FROM ets.year_month_day) = ?3 ) AND om.designation = 'KD' "
-			+ "AND om.id_employee = ?4 ",
-			nativeQuery = true)
-	OrderMission findByDateAndOrderWorkMissionIDAndDesignation(int day, int month, int year, int employeeId);
+    @Query(value = "CALL FindByDateAndOrderWorkMissionIDAndDesignation(:day, :month, :year, :employeeId);", nativeQuery = true)
+    OrderMission findByDateAndOrderWorkMissionIDAndDesignation(@Param("day") int day,
+                                                               @Param("month") int month,
+                                                               @Param("year") int year,
+                                                               @Param("employeeId") int employeeId);
 
-	
 
 }
