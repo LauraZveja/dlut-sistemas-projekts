d9a446a92641143a0dbcaccb648ffe2e038e69ed
Laura <lauramaisele@hotmail.lv>
2024-02-29 14:26:46 +0200

    EDIT: Change IOrderVacationRepo from regular query to sql procedure

--- src/main/java/eu/virac/dlut/repos/IOrderVacationRepo.java
+++ src/main/java/eu/virac/dlut/repos/IOrderVacationRepo.java
@@ -4,14 +4,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import eu.virac.dlut.models.OrderVacation;
+import org.springframework.data.repository.query.Param;
 
 public interface IOrderVacationRepo extends CrudRepository<OrderVacation, Integer>{
 
-	@Query(value = "SELECT * FROM order_vacation ov "
-			+ "JOIN employee_time_sheet ets ON ov.id_order = ets.id_ord_vacation "
-			+ "WHERE (EXTRACT(DAY FROM ets.year_month_day) = ?1 AND EXTRACT(MONTH FROM ets.year_month_day) = ?2 AND EXTRACT(YEAR FROM ets.year_month_day) = ?3 ) AND ov.designation = 'AI' "
-			+ "AND ov.id_employee = ?4 ",
-			nativeQuery = true)
-	OrderVacation findByDateAndOrderVacationIDAndDesignation(int day, int month, int year, int employeeId);
+	@Query(value = "CALL FindByDateAndOrderVacationIDAndDesignation(:day, :month, :year, :employeeId);", nativeQuery = true)
+	OrderVacation findByDateAndOrderVacationIDAndDesignation(@Param("day") int day,
+															 @Param("month") int month,
+															 @Param("year") int year,
+															 @Param("employeeId") int employeeId);
+
 
 }
