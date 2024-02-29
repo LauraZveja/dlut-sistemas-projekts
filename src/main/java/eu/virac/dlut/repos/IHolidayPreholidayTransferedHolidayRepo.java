16542c0984ad92219e7a93f6da359c7c8c98d8e3
Laura <lauramaisele@hotmail.lv>
2024-02-29 14:05:59 +0200

    EDIT: Change IHolidayPreholidayTransferedHolidayRepoSQLProcedureRepo from regular query to sql procedure

--- src/main/java/eu/virac/dlut/repos/IHolidayPreholidayTransferedHolidayRepo.java
+++ src/main/java/eu/virac/dlut/repos/IHolidayPreholidayTransferedHolidayRepo.java
@@ -4,36 +4,35 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import eu.virac.dlut.models.HolidayPreholidayTransferedHoliday;
+import org.springframework.data.repository.query.Param;
 
 public interface IHolidayPreholidayTransferedHolidayRepo extends CrudRepository<HolidayPreholidayTransferedHoliday, Integer>{
 
 	
 	//search and join looking also for title "Sveetki"
-	@Query(value = "SELECT * FROM holiday_preholiday_transfered_holiday h "
-			+ "JOIN day_status d ON h.id_day_status = d.id_day_status "
-			+ "WHERE (EXTRACT(DAY FROM h.date) = ?1 AND EXTRACT(MONTH FROM h.date) = ?2 AND EXTRACT(YEAR FROM h.date) = ?3 ) AND d.day_title LIKE 'Sv%'",
-			nativeQuery = true)
-	HolidayPreholidayTransferedHoliday findByDateAndHolidayTitle(int day, int month, int year);
+	@Query(value = "CALL FindByDateAndHolidayTitle(:day, :month, :year);", nativeQuery = true)
+	HolidayPreholidayTransferedHoliday findByDateAndHolidayTitle(@Param("day") int day,
+																 @Param("month") int month,
+																 @Param("year") int year);
 
-	
+
+
 	//search and join looking also for title "Pārcelta-D"
-	@Query(value = "SELECT * FROM holiday_preholiday_transfered_holiday h "
-			+ "JOIN day_status d ON h.id_day_status = d.id_day_status "
-			+ "WHERE (EXTRACT(DAY FROM h.date) = ?1 AND EXTRACT(MONTH FROM h.date) = ?2 AND EXTRACT(YEAR FROM h.date) = ?3 ) AND d.day_title='Pārcelta-D'",
-			nativeQuery = true)
-	HolidayPreholidayTransferedHoliday findByDateAndTransferedWorkDayRemark(int day, int month, int year);
-	
+	@Query(value = "CALL FindByDateAndTransferredWorkDayRemark(:day, :month, :year);", nativeQuery = true)
+	HolidayPreholidayTransferedHoliday findByDateAndTransferedWorkDayRemark(@Param("day") int day,
+																			@Param("month") int month,
+																			@Param("year") int year);
+
 	//search and join looking also for title "Pārcelta-B"
-	@Query(value = "SELECT * FROM holiday_preholiday_transfered_holiday h "
-			+ "JOIN day_status d ON h.id_day_status = d.id_day_status "
-			+ "WHERE (EXTRACT(DAY FROM h.date) = ?1 AND EXTRACT(MONTH FROM h.date) = ?2 AND EXTRACT(YEAR FROM h.date) = ?3 ) AND d.day_title='Pārcelta-B'",
-			nativeQuery = true)
-	HolidayPreholidayTransferedHoliday findByDateAndTransferedHolidayRemark(int day, int month, int year);
-	
+	@Query(value = "CALL FindByDateAndTransferredHolidayRemark(:day, :month, :year);", nativeQuery = true)
+	HolidayPreholidayTransferedHoliday findByDateAndTransferedHolidayRemark(@Param("day") int day,
+																			@Param("month") int month,
+																			@Param("year") int year);
+
 	//search and join looking also for title "Pārcelta-B"
-	@Query(value = "SELECT * FROM holiday_preholiday_transfered_holiday h "
-			+ "JOIN day_status d ON h.id_day_status = d.id_day_status "
-			+ "WHERE (EXTRACT(DAY FROM h.date) = ?1 AND EXTRACT(MONTH FROM h.date) = ?2 AND EXTRACT(YEAR FROM h.date) = ?3 ) AND d.day_title='Pirmssvētku'",
-			nativeQuery = true)
-	HolidayPreholidayTransferedHoliday findByDateAndPreHolidya(int day, int month, int year);
+	@Query(value = "CALL FindByDateAndPreHoliday(:day, :month, :year);", nativeQuery = true)
+	HolidayPreholidayTransferedHoliday findByDateAndPreHolidya(@Param("day") int day,
+															   @Param("month") int month,
+															   @Param("year") int year);
+
 }
