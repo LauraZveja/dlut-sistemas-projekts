a64ae0c31d4ffedf4701a36abfc8d5b6cebae5d9
Laura <lauramaisele@hotmail.lv>
2024-02-29 11:53:48 +0200

    EDIT: Change IFinanceOrderWorkRepo from regular query to sql procedure

--- src/main/java/eu/virac/dlut/repos/IFinanceOrderWorkRepo.java
+++ src/main/java/eu/virac/dlut/repos/IFinanceOrderWorkRepo.java
@@ -6,41 +6,36 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import eu.virac.dlut.models.FinanceOrderWork;
+import org.springframework.data.repository.query.Param;
 
 public interface IFinanceOrderWorkRepo extends CrudRepository<FinanceOrderWork, Integer>{
 
-	@Query(value = "SELECT fow.position_in_project FROM finance_order_work fow "
-			+ "JOIN order_work ow ON fow.id_ord_work=ow.id_order "
-			+ "JOIN employee emp ON ow.id_employee=emp.id_employee "
-			+ "WHERE emp.id_employee = ?1 AND fow.id_project = ?2 ", nativeQuery = true)
-	String getPositionInProjectForEmployee(int idEmployee, int idFinSource);
+	@Query(value = "CALL GetPositionInProjectForEmployee(:idEmployee, :idFinSource);", nativeQuery = true)
+	String getPositionInProjectForEmployee(@Param("idEmployee") int idEmployee,
+										   @Param("idFinSource") int idFinSource);
+
 
 	FinanceOrderWork findByOrdWorkIdOrder(int ordWorkId);
 
-	@Query(value = "SELECT fow.pay_per_hour FROM finance_order_work fow "
-			+ "JOIN order_work ow ON fow.id_ord_work=ow.id_order "
-			+ "JOIN employee emp ON ow.id_employee=emp.id_employee "
-			+ "WHERE emp.id_employee = ?1 AND fow.id_project = ?2 ", nativeQuery = true)
-	double getPayPerHourByEmployeeAndProject(int employeeId, int projectId);
-	
-	@Query(value = "SELECT fow.pay_per_hour FROM finance_order_work fow "
-			+ "JOIN order_work ow ON fow.id_ord_work=ow.id_order "
-			+ "JOIN employee emp ON ow.id_employee=emp.id_employee "
-			+ "WHERE emp.id_employee = ?1 AND (fow.id_project = ?2 OR fow.id_base_fin = ?2 OR fow.id_ac_work_load = ?2 OR fow.id_indirectvuas = ?2 )", nativeQuery = true)
-	double getPayPerHourByEmployeeInFinSource(int employeeId, int finSourceId);
+	@Query(value = "CALL GetPayPerHourByEmployeeAndProject(:employeeId, :projectId);", nativeQuery = true)
+	double getPayPerHourByEmployeeAndProject(@Param("employeeId") int employeeId,
+											 @Param("projectId") int projectId);
+
+
+	@Query(value = "CALL GetPayPerHourByEmployeeInFinSource(:employeeId, :finSourceId);", nativeQuery = true)
+	double getPayPerHourByEmployeeInFinSource(@Param("employeeId") int employeeId,
+											  @Param("finSourceId") int finSourceId);
+
 
 	//ArrayList<FinanceOrderWork> findByIdBaseFinNotNull();
 
-	@Query(value = "SELECT * FROM finance_order_work fow "
-			+ "JOIN order_work ow ON fow.id_ord_work=ow.id_order "
-			+ "JOIN employee emp ON ow.id_employee=emp.id_employee "
-			+ "WHERE emp.id_employee = ?1 AND (fow.id_project = ?2 OR fow.id_base_fin = ?2 OR fow.id_ac_work_load = ?2 OR fow.id_indirectvuas = ?2 ) AND fow.position_in_project = ?3 ", nativeQuery = true)
-	FinanceOrderWork getFinanceOrderWorkByEmployeeIdAndFinSourceTitleAndPosition(int idEmployee, int financeSourceId, String Position);
+	@Query(value = "CALL GetFinanceOrderWorkByEmployeeAndDetails(:employeeId, :financeSourceId, :position);", nativeQuery = true)
+	FinanceOrderWork getFinanceOrderWorkByEmployeeIdAndFinSourceTitleAndPosition(@Param("employeeId") int employeeId,
+																				 @Param("financeSourceId") int financeSourceId,
+																				 @Param("position") String Position);
 
-	@Query(value = "SELECT * FROM finance_order_work fow "
-			+ "JOIN order_work ow ON fow.id_ord_work=ow.id_order "
-			+ "JOIN employee emp ON ow.id_employee=emp.id_employee "
-			+ "WHERE emp.id_employee = ?1 "
-			+ "AND (fow.id_project = ?2 OR fow.id_base_fin = ?2 OR fow.id_ac_work_load = ?2 OR fow.id_indirectvuas = ?2 ) ", nativeQuery = true)
-	FinanceOrderWork getByEmployeeIdAndFinanceSourceTitle(int employeeId, int financeSourceId);
+	@Query(value = "CALL GetByEmployeeIdAndFinanceSource(:employeeId, :financeSourceId);", nativeQuery = true)
+	FinanceOrderWork getByEmployeeIdAndFinanceSourceTitle(@Param("employeeId") int employeeId,
+														  @Param("financeSourceId") int financeSourceId);
+
 }
