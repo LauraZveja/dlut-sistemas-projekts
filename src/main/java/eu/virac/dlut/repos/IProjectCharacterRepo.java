032214d80a99e09a00535d9d0d07274f9527ef0e
Laura <lauramaisele@hotmail.lv>
2024-02-29 14:29:42 +0200

    EDIT: Change IProjectCharacterRepo from regular query to sql procedure

--- src/main/java/eu/virac/dlut/repos/IProjectCharacterRepo.java
+++ src/main/java/eu/virac/dlut/repos/IProjectCharacterRepo.java
@@ -4,12 +4,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import eu.virac.dlut.models.ProjectCharacter;
+import org.springframework.data.repository.query.Param;
 
 public interface IProjectCharacterRepo extends CrudRepository<ProjectCharacter, Integer> {
 
-	@Query(value = "SELECT pch.proj_char_title FROM project_character pch "
-			+ "JOIN project p ON p.id_proj_char=pch.id_proj_char "
-			+ "WHERE p.id_fin_source = ?1 ", nativeQuery = true)
-	String findProjectCharacterByProjId(int projectId);
+    @Query(value = "CALL FindProjectCharacterByProjId(:projectId);", nativeQuery = true)
+    String findProjectCharacterByProjId(@Param("projectId") int projectId);
+
 
 }
