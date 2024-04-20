c6a839b1322d14270467ed7e8426dc724f5f6770
Laura <lauramaisele@hotmail.lv>
2024-04-20 19:35:14 +0300

    NEW: angular frontend for DLUT controller

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { ViewAllEmployeesComponent } from './components/employee/view-all-employ
 import { NgModule } from '@angular/core';
 import { UpdateEmployeeComponent } from './components/employee/update-employee/update-employee.component';
 import { CreateEmployeeComponent } from './components/employee/create-employee/create-employee.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'employees', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'employees', component: ViewAllEmployeesComponent},
     { path: 'update-employee', component: UpdateEmployeeComponent },
-    { path: 'create-employee', component: CreateEmployeeComponent }
+    { path: 'create-employee', component: CreateEmployeeComponent },
+    { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
