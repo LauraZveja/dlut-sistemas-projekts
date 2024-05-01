4fc10998bf2956b4f70c464b08938cab835b7fa1
Laura <lauramaisele@hotmail.lv>
2024-05-01 10:33:48 +0300

    ADD: sql file for manually altering column names in inherited tables

---
+++ src/main/resources/migrate/migrateColumnNames.sql
@@ -0,0 +1,15 @@
+ALTER TABLE project RENAME COLUMN id_fin_source TO id_project //
+
+ALTER TABLE order_mission RENAME COLUMN id_order TO id_ord_mission //
+
+ALTER TABLE order_other RENAME COLUMN id_order TO id_ord_other //
+
+ALTER TABLE order_vacation RENAME COLUMN id_order TO id_ord_vacation //
+
+ALTER TABLE order_work RENAME COLUMN id_order TO id_ord_work //
+
+ALTER TABLE academic_work_load RENAME COLUMN id_fin_source TO id_ac_work_load //
+
+ALTER TABLE base_fin RENAME COLUMN id_fin_source TO id_base_fin //
+
+ALTER TABLE indirect_vuas RENAME COLUMN id_fin_source TO id_indirect_vuas //
