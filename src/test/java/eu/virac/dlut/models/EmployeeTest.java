aecc0b72ab8cf3a17e871c9cf143e4343d187562
Laura <lauramaisele@hotmail.lv>
2024-06-02 15:15:35 +0300

    Update: Employee tests

--- src/test/java/eu/virac/dlut/models/EmployeeTest.java
+++ src/test/java/eu/virac/dlut/models/EmployeeTest.java
@@ -5,15 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-public class EmployeeTest {
+class EmployeeTest {
 
     Department d1 = new Department("Augstas veiktspējas skaitļošanas nodaļa", "HCP");
 
     Employee emp1 = new Employee();
     Employee e1 = new Employee("Karina", "Šķirmante", "Pētniece", true, "P77/1, 2011.11.12", d1);
+    Department department = new Department("Departmentaments", "Abreviatūra");
+    Employee employee = new Employee("Džons", "Stirna", "Pētnieks", true, "P77/1, 2024.11.12", department);
 
     @Test
-    public void testEmployee(){
+    void testEmployee(){
         assertEquals("Karina", e1.getName());
         assertEquals("Šķirmante", e1.getSurname());
         assertEquals("Pētniece", e1.getPosition());
@@ -23,7 +25,7 @@ public class EmployeeTest {
     }
 
     @Test
-    public void testEmployeeSetters(){
+    void testEmployeeSetters(){
         emp1.setName("Karina");
         emp1.setSurname("Šķirmante");
         emp1.setPosition("Pētniece");
@@ -40,11 +42,40 @@ public class EmployeeTest {
     }
 
     @Test
-    public void testEmployeeToString() {
+    void testEmployeeToString() {
         String expected = "Employee(idEmployee=0, name=Karina, surname=Šķirmante, position=Pētniece, isElected=true, workContractNoDate=P77/1, 2011.11.12, department=Department(idDepartment=0, title=Augstas veiktspējas skaitļošanas nodaļa, abbreviation=HCP, isDeleted=false), isDeleted=false)";
         assertEquals(expected, e1.toString());
     }
-    
+
+    @Test
+    void testAddFinancePlanningToEmployee() {
+
+        FinancePlanning financePlanning = new FinancePlanning();
+
+        employee.addFinancePlanningToEmployee(financePlanning);
+
+        assertNotNull(employee.getFinPlanningCol());
+        assertTrue(employee.getFinPlanningCol().contains(financePlanning));
+    }
+
+    @Test
+    void testRemoveFinancePlanningFromEmployee() {
+        FinancePlanning financePlanning = new FinancePlanning();
+
+        employee.addFinancePlanningToEmployee(financePlanning);
+        employee.removeFinancePlanningFromEmployee(financePlanning);
+
+        assertNotNull(employee.getFinPlanningCol());
+        assertFalse(employee.getFinPlanningCol().contains(financePlanning));
+    }
+
+    @Test
+    void testFinPlanningColInitialization() {
+        Employee employee = new Employee();
+        assertNotNull(employee.getFinPlanningCol());
+        assertTrue(employee.getFinPlanningCol().isEmpty());
+    }
+
 
 
 }
