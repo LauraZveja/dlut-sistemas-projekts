8e12f08e92dca637e9582baef248dd2aca85665e
Laura <lauramaisele@hotmail.lv>
2024-06-08 02:27:46 +0300

    update tests

--- src/test/java/eu/virac/dlut/DlutApplicationTests.java
+++ src/test/java/eu/virac/dlut/DlutApplicationTests.java
@@ -1,18 +1,16 @@
 package eu.virac.dlut;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DlutApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertNotNull(SpringApplication.run(DlutApplication.class));
+
 	}
 
 }
